9c147994216ff1f2964184c3a8f39c9baf4aa6f9
Dobras Timothy <GumekTV@gmail.com>
2024-06-24 04:28:30 +0300

    kujhg

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Head from "next/head";
 
 const defaultUrl = process.env.VERCEL_URL
-	? `${process.env.VERCEL_URL}`
+	? `https://${process.env.VERCEL_URL}`
 	: "http://localhost:3000";
 
 export const metadata = {
