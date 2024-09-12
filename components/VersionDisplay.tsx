979884afca1eeb9c74e742caba15d74b22410745
Dobras Timothy <GumekTV@gmail.com>
2024-09-12 14:15:11 +0300

    version VersionDisplay

--- components/VersionDisplay.tsx
+++ components/VersionDisplay.tsx
@@ -1,5 +1,5 @@
 export default function VersionDisplay() {
-  const version = process.env.VERCEL_GIT_COMMIT_SHA || "dev";
+  const version = process.env.VERCEL_GIT_COMMIT_SHA || "0.169";
 
   return <p className="">Version: {version.slice(-5)}</p>;
 }
