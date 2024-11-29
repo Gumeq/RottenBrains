52ba2bef9a37de159db48d3760f05f4c37486cfe
Dobras Timothy <GumekTV@gmail.com>
2024-11-30 00:44:54 +0200

    bugfix to themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import ThemeScript from "@/components/ThemeScript";
 import { Toaster } from "@/components/ui/toaster";
-import CookieConsent from "@/components/CookieConsent"; // Import the CookieConsent component
-import Head from "next/head";
+import CookieConsent from "@/components/CookieConsent";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
@@ -24,16 +23,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <Head>
+      <head>
         <ThemeScript /> {/* Include ThemeScript in the head */}
-      </Head>
+      </head>
       <UserProvider>
         <body className="overflow-x-hidden bg-background text-foreground">
-          <main>{children}</main>
+          <main className="">{children}</main>
           <Toaster />
           <Analytics />
+          <CookieConsent></CookieConsent>
           <SpeedInsights />
-          <CookieConsent /> {/* Add the CookieConsent component here */}
         </body>
       </UserProvider>
     </html>
