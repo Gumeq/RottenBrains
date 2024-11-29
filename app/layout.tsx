4992a1d4a3154bf1a5e7ec66ed0bee0a66c18070
Dobras Timothy <GumekTV@gmail.com>
2024-11-30 01:26:48 +0200

    bugix themes again again

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ const defaultUrl = process.env.VERCEL_URL
 import UserProvider from "@/context/UserContext";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import ThemeScript from "@/components/ThemeScript";
 import { Toaster } from "@/components/ui/toaster";
 import CookieConsent from "@/components/CookieConsent";
 
@@ -15,11 +14,35 @@ export const metadata = {
   title: "RottenBrains",
   description: "Your Hub for Movie Reviews and Streaming!",
 };
+
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en" className={GeistSans.className}>
       <head>
-        <ThemeScript /> {/* Include ThemeScript in the head */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  var theme = 'light';
+                  var savedTheme = localStorage.getItem('theme');
+                  if (savedTheme) {
+                    theme = savedTheme;
+                  } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    theme = 'dark';
+                  }
+                  if (theme === 'dark') {
+                    document.documentElement.classList.add('dark');
+                  } else {
+                    document.documentElement.classList.remove('dark');
+                  }
+                } catch (e) {
+                  console.error('Error setting theme:', e);
+                }
+              })();
+            `,
+          }}
+        />
       </head>
       <UserProvider>
         <body className="overflow-x-hidden bg-background text-foreground">
