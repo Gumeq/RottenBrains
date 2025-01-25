d972d7fc8d75c764db427755551c67fcba903488
Dobras Timothy <GumekTV@gmail.com>
2025-01-25 14:15:12 +0200

    bugfix ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,20 @@ import UserProvider from "@/context/UserContext";
 import { cookies } from "next/headers";
 import { Theme, ThemeProvider } from "@/context/ThemeContext";
 import LegalConsent from "@/components/LegalConsent";
-import Head from "next/head";
 
 export const metadata = {
   title: "RottenBrains",
   description: "Your Hub for Movie Reviews and Streaming!",
+  other: {
+    // Add the AdSense script as part of the metadata API
+    script: [
+      {
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4557341861686356",
+        async: true,
+        crossOrigin: "anonymous",
+      },
+    ],
+  },
 };
 
 export default async function RootLayout({
@@ -29,13 +38,6 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={`${GeistSans.className} ${themeCookie}`}>
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4557341861686356"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
       <body className="overflow-x-hidden bg-background text-foreground">
         <UserProvider>
           <ThemeProvider initialTheme={themeCookie}>
