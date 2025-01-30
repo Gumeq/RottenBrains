b3a676181f5aec2f05476537fd81dd32624702a3
Dobras Timothy <GumekTV@gmail.com>
2025-01-30 04:29:37 +0200

    Update Beta 0.275

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,48 @@
+import { getPopular, getTrendingMovies, getTrendingTV } from "@/utils/tmdb";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const movies = await getTrendingMovies();
+  const tv = await getTrendingTV();
+
+  // 2. Get your base url (e.g., from .env or a fallback)
+  const baseUrl = process.env.SITE_URL || "https://www.rotten-brains.com";
+
+  // 3. Build the XML sitemap
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset 
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 
+                      http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+>
+  ${movies.results
+    .map((media: any) => {
+      return `
+    <url>
+      <loc>${baseUrl}/watch/movie/${media.id}</loc>
+      <priority>0.80</priority>
+      <changefreq>daily</changefreq>
+    </url>`;
+    })
+    .join("")}
+      ${tv.results
+        .map((media: any) => {
+          return `
+    <url>
+      <loc>${baseUrl}/watch/tv/${media.id}/1/1</loc>
+      <priority>0.80</priority>
+      <changefreq>daily</changefreq>
+    </url>`;
+        })
+        .join("")}
+</urlset>`;
+
+  // 4. Return the response with correct headers
+  return new Response(sitemap, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
